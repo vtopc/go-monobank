e3cf164e154459f7415965ac9efa3bc1a0296f0f
Vilen <32271530+vtopc@users.noreply.github.com>
2019-07-06 21:33:19 +0300

    removed redundant

--- types.go
+++ types.go
@@ -1,19 +1,14 @@
 package monobank
 
 import (
-	"fmt"
 	"strconv"
 	"time"
-
-	"github.com/rmg/iso4217"
 )
-
-type Time time.Time
 
 // ClientInfo - client/user info
 type ClientInfo struct {
-	Name     string    `json:"name"`
-	Accounts []Account `json:"accounts"`
+	Name     string   `json:"name"`
+	Accounts Accounts `json:"accounts"`
 }
 
 type Account struct {
@@ -23,6 +18,8 @@ type Account struct {
 	CurrencyCode int    `json:"currencyCode"`
 	CashbackType string `json:"cashbackType"` // enum: None, UAH, Miles
 }
+
+type Accounts []Account
 
 // Statement - bank account statement
 type Statement struct {
@@ -42,35 +39,7 @@ type Statement struct {
 // Statements - transactions
 type Statements []Statement
 
-func (c *ClientInfo) String() string {
-	if c == nil {
-		return "<nil>"
-	}
-
-	resp := "І'мя: " + c.Name + "\n"
-	for _, a := range c.Accounts {
-		resp = resp + "🔸🔸🔸\n" + a.String()
-	}
-
-	return resp
-}
-
-func (a *Account) String() string {
-	currency, minorUnits := iso4217.ByCode(a.CurrencyCode)
-	if len(currency) == 0 {
-		// setting defaults:
-		currency = strconv.Itoa(a.CurrencyCode)
-		minorUnits = 2
-	}
-
-	return "Account: /ST_" + a.ID + "\n" +
-		"Валюта: " + currency + "\n" +
-		// "Кешбек: " + a.CashbackType + "\n" +
-		// TODO: rewrite to string concat:
-		fmt.Sprintf("Власні кошти: %s %s\n", ToBanknote(a.Balance-a.CreditLimit, minorUnits), currency) +
-		fmt.Sprintf("Баланс: %s %s\n", ToBanknote(a.Balance, minorUnits), currency) +
-		fmt.Sprintf("Кредитний ліміт: %s %s\n", ToBanknote(a.CreditLimit, minorUnits), currency)
-}
+type Time time.Time
 
 func (t *Time) UnmarshalJSON(data []byte) error {
 	ts, err := strconv.ParseInt(string(data), 10, 64)
