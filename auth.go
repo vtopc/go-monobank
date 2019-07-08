5d2e963b31086b4304d234e9dd2239c82ec426c5
Vilen <32271530+vtopc@users.noreply.github.com>
2019-07-08 22:27:53 +0300

    added Sign example

--- auth.go
+++ auth.go
@@ -43,3 +43,9 @@ func NewNoopAuthorizer() NoopAuthorizer {
 // 	requestID string // X-Request-Id
 // 	sign      string // X-Sign (X-time | X-Request-Id | URL)
 // }
+//
+// Sign (X-time | X-Request-Id | URL)
+//  - X-time=1561560962 (Wed, 26 Jun 2019 14:56:02 GMT)
+//  - X-Request-Id=acW5k2ERnupgnWFyBkCY0nA
+//  - URL=/personal/client-info
+//  Sign (1561560962acW5k2ERnupgnWFyBkCY0nA/personal/client-info)=eaBHn+T18kr7w6uAzLJ037o1w/JMpAfV81yNaHaXJUxv9bbi/cORuA0gGsazwG+VxCq2Y+TmIb81zbGbuiaRQA==
