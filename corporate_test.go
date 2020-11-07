7030c6d6ab6af8b5b9bcf131de90e2a8ba955510
vtopc <32271530+vtopc@users.noreply.github.com>
2020-11-07 18:46:49 +0200

    added TestCorporateClient_withAuth

--- corporate_test.go
+++ corporate_test.go
@@ -1,12 +1,25 @@
-package monobank_test
+package monobank
 
 import (
 	"testing"
 
-	"github.com/vtopc/go-monobank"
+	"github.com/stretchr/testify/assert"
 )
 
 // checks that Client satisfies interface
 func TestClient_CorporateClient(t *testing.T) {
-	var _ monobank.CorporateAPI = monobank.CorporateClient{}
+	var _ CorporateAPI = CorporateClient{}
+}
+
+func TestCorporateClient_withAuth(t *testing.T) {
+	c := CorporateClient{
+		commonClient: newCommonClient(nil),
+	}
+
+	auth := CorpAuth{
+		requestID: "123",
+	}
+
+	authClient := c.withAuth(auth)
+	assert.Equal(t, auth, authClient.auth)
 }
