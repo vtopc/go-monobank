f49c8809525f3befad52b137005a357c243a72a9
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2023-05-30 20:53:40 +0300

    Comments (#33)

--- types.go
+++ types.go
@@ -51,23 +51,30 @@ type Jars []Jar
 
 // Transaction - bank account statement
 type Transaction struct {
-	ID              string        `json:"id"`
-	Time            epoch.Seconds `json:"time"`
-	Description     string        `json:"description"`
-	MCC             int32         `json:"mcc"`
-	OriginalMCC     int32         `json:"originalMcc"`
-	Hold            bool          `json:"hold"`
-	Amount          int64         `json:"amount"`
-	OperationAmount int64         `json:"operationAmount"`
-	CurrencyCode    int           `json:"currencyCode"`
-	CommissionRate  int64         `json:"commissionRate"`
-	CashbackAmount  int64         `json:"cashbackAmount"`
-	Balance         int64         `json:"balance"`
-	Comment         string        `json:"comment"`
-	ReceiptID       string        `json:"receiptId"`
-	InvoiceID       string        `json:"invoiceId"`
-	EDRPOU          string        `json:"counterEdrpou"`
-	IBAN            string        `json:"counterIban"`
+	ID          string        `json:"id"`
+	Time        epoch.Seconds `json:"time"`
+	Description string        `json:"description"`
+	MCC         int32         `json:"mcc"`
+	OriginalMCC int32         `json:"originalMcc"`
+	Hold        bool          `json:"hold"`
+	// Amount in the account currency
+	Amount int64 `json:"amount"`
+	// OperationAmount in the transaction currency or amount after double conversion
+	OperationAmount int64 `json:"operationAmount"`
+	// ISO 4217 numeric code
+	CurrencyCode   int    `json:"currencyCode"`
+	CommissionRate int64  `json:"commissionRate"`
+	CashbackAmount int64  `json:"cashbackAmount"`
+	Balance        int64  `json:"balance"`
+	Comment        string `json:"comment"`
+	// For withdrawal only.
+	ReceiptID string `json:"receiptId"`
+	// For fop(ФОП) accounts only.
+	InvoiceID string `json:"invoiceId"`
+	// For fop(ФОП) accounts only.
+	EDRPOU string `json:"counterEdrpou"`
+	// For fop(ФОП) accounts only.
+	IBAN string `json:"counterIban"`
 }
 
 // Transactions - transactions
